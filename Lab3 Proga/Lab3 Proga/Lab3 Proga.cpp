c56a3069ba3bc5edc4a7b3825ebdc1697d08f020
Роберт Шевцов <55320224+RobertUA@users.noreply.github.com>
2020-04-23 22:11:50 +0300

    BEST

--- Lab3 Proga/Lab3 Proga/Lab3 Proga.cpp
+++ Lab3 Proga/Lab3 Proga/Lab3 Proga.cpp
@@ -1,42 +1,28 @@
-﻿#include<fstream>
+﻿#include <fstream>
 #include <iostream>
-#include<string>
-#include<algorithm>
-#include<vector>
-#include<iomanip>
+#include <string>
+//#include<algorithm>
+#include <vector>
+
+using namespace std;
+
 struct Vertex
 {
 	int i, j;
 	int ksos = 0;// кількість sosedi'в
 	int sosedi[8] = { -1 };
 };
+
+void sol(Vertex* mVert, int** mm, int k, int kk);
 char symb(int N);
-using namespace std;
+void lout(int** mm, int k, int kk);
+void reconstructpath(int* prev, vector<int>& c, int x, int y);
+int h(int x, int y, int** mm, int k, int kk);
+void file_out(vector<int>& c, int** mm, int k, int kk);
+void A_star(Vertex* node, int k, int kk, int** mm, int* prev, int ss, int x, int y);
+
 int curr;
-int h(int x, int y, int** mm, int k, int kk)
-{
-	int x1, x2, y1, y2;
-	for (int i = 0; i < k; i++)
-	{
-		for (int j = 0; j < kk; j++)
-		{
-			if (mm[i][j] == x)
-			{
-				x1 = i; x2 = j;
-			}
-			if (mm[i][j] == y)
-			{
-				y1 = i; y2 = j;
-			}
-		}
-	}
-	int dx = abs(x1 - y1);
-	int dy = abs(x2 - y2);
-	//return 4 * (dx + dy) + (4 - 2 * 8) * min(dx, dy);
-	//return sqrt(dx * dx + dy * dy);
-	//return(dx * dx + dy * dy);
-	return (dx + dy);
-}
+
 class Queue
 {
 	int* Va; int* Po;	//to store elements
@@ -88,97 +74,6 @@ public:
 	}
 };
 
-void lout(int** mm, int k, int kk);
-
-void A_star(Vertex* node, int k, int kk, int** mm, int* prev, int ss, int x, int y)
-{
-	Queue Q(ss);
-	int* g = new int[ss];
-	bool* used = new bool[ss];
-	int* f = new int[ss];
-	for (int i = 0; i < ss; i++) { g[i] = INT_MAX; f[i] = 0; used[i] = false; }
-	g[y]=INT_MAX;
-	g[x] = 0;
-	f[x] = g[x] + h(x, y, mm, k, kk);
-	Q.push(x, f[x]);
-	while (!Q.empty())
-	{
-		int current = Q.getmin();
-		used[current] = true;
-	
-		if (current == y)
-		{
-			cout << "END: ";
-			break;
-		}
-		Q.pop();
-		for (int i = 0; i < node[current - 1].ksos; i++)
-		{
-			if (g[node[current-1].sosedi[i]] > g[current] + 1)
-			{
-				g[node[current-1].sosedi[i]] = g[current] + 1;
-				prev[node[current - 1].sosedi[i]] = current;
-				f[node[current - 1].sosedi[i]] = g[node[current - 1].sosedi[i]] + h(node[current - 1].sosedi[i], y, mm, k, kk);
-				if (!used[node[current - 1].sosedi[i]])
-				{
-					Q.push(node[current - 1].sosedi[i], f[node[current - 1].sosedi[i]]);
-				}
-			}
-		}
-	}
-}
-void reconstructpath(int* prev, vector<int>& c, int x, int y)
-{
-	curr = y;
-
-	c.push_back(curr);
-	while (curr != x &&curr>0)
-	{
-		curr = prev[curr];
-		c.push_back(curr);
-	}	
-	c.push_back(x);
-	reverse(c.begin(), c.end());
-}
-void file_out(vector<int>& c, int** mm, int k, int kk)
-{
-	ofstream fout;
-	fout.open("d:\\output.txt");
-	bool is;
-	
-	if (curr == 0) { cout << "No Path"; }
-	else
-	{
-		for (int i = 0; i < k; i++)
-		{
-			cout << "\n | ";
-			for (int j = 0; j < kk; j++)
-			{
-				is = false;
-				for (int o = 0; o < c.size(); o++)
-				{
-					if (mm[i][j] == c[o])
-					{
-						is = true;
-
-						cout << symb(o) << ' '; fout << symb(o) << ' ';
-						break;
-					}
-
-				}
-				if (is == false)
-				{
-					if (mm[i][j] == 0) { cout << "X ";	fout << "X "; }
-					else { cout << "  ";	fout << "  "; }
-				}
-
-			}
-			fout << endl;
-			cout << "|";
-		}
-	}
-}
-
 int main()
 {
 	string fff = "";
@@ -208,17 +103,141 @@ int main()
 	}
 	Vertex* mVert = new Vertex[ss];
 	lout(mm, k, kk);
-	int Sv, Ev, Ei, Ej;
+	int Sv, Ev;
 	cout << "\n Start vertex: ";
 	cin >> Sv;
+	if (Sv > ss || Sv < 1) { cout << " [!] Wrong vertex"; return -1; }
 	cout << "\n Finish vertex: ";
 	cin >> Ev;
+	if (Ev > ss || Ev < 1) { cout << " [!] Wrong vertex"; return -1; }
+	///
+	sol(mVert, mm, k, kk);
+	///
+	int* prev = new int[ss];
+	for (int i = 0; i < ss; i++) prev[i] = 0;
+	vector<int>c;
+	A_star(mVert, k, kk, mm, prev, ss, Sv, Ev);
+	reconstructpath(prev, c, Sv, Ev);
+	file_out(c, mm, k, kk);
+	///
+}
+
+void reconstructpath(int* prev, vector<int>& c, int x, int y)
+{
+	curr = y;
+
+	c.push_back(curr);
+	while (curr != x && curr > 0)
+	{
+		curr = prev[curr];
+		c.push_back(curr);
+	}
+	c.push_back(x);
+	reverse(c.begin(), c.end());
+}
+int h(int x, int y, int** mm, int k, int kk)
+{
+	int x1, x2, y1, y2;
+	for (int i = 0; i < k; i++)
+	{
+		for (int j = 0; j < kk; j++)
+		{
+			if (mm[i][j] == x)
+			{
+				x1 = i; x2 = j;
+			}
+			if (mm[i][j] == y)
+			{
+				y1 = i; y2 = j;
+			}
+		}
+	}
+	int dx = abs(x1 - y1);
+	int dy = abs(x2 - y2);
+	//return 4 * (dx + dy) + (4 - 2 * 8) * min(dx, dy);
+	//return sqrt(dx * dx + dy * dy);
+	//return(dx * dx + dy * dy);
+	return (dx + dy);
+	//return 1;
+}
+void A_star(Vertex* node, int k, int kk, int** mm, int* prev, int ss, int x, int y)
+{
+	Queue Q(ss);
+	int* g = new int[ss];
+	bool* used = new bool[ss];
+	int* f = new int[ss];
+	for (int i = 0; i < ss; i++) { g[i] = INT_MAX; f[i] = 0; used[i] = false; }
+	g[y] = INT_MAX;
+	g[x] = 0;
+	f[x] = g[x] + h(x, y, mm, k, kk);
+	Q.push(x, f[x]);
+	while (!Q.empty())
+	{
+		int current = Q.getmin();
+		used[current] = true;
+		if (current == y) break;////
+		Q.pop();
+		for (int i = 0; i < node[current - 1].ksos; i++)
+		{
+			if (g[node[current - 1].sosedi[i]] > g[current] + (node[current - 1].i == node[node[current - 1].sosedi[i] - 1].i || node[current - 1].j == node[node[current - 1].sosedi[i] - 1].j ? 10000000 : 14142135))
+			{
+				//cout << (node[current - 1].i == node[node[current - 1].sosedi[i] - 1].i || node[current - 1].j == node[node[current - 1].sosedi[i] - 1].j ? 10000000 : 14142135) << " " << node[current-1].i << " " << node[current-1].j << " " << node[node[current-1].sosedi[i]-1].i << " " << node[node[current-1].sosedi[i]-1].j << "\n";
+				g[node[current - 1].sosedi[i]] = g[current] + (node[current - 1].i == node[node[current - 1].sosedi[i] - 1].i || node[current - 1].j == node[node[current - 1].sosedi[i] - 1].j ? 10000000 : 14142135);
+				prev[node[current - 1].sosedi[i]] = current;
+				f[node[current - 1].sosedi[i]] = g[node[current - 1].sosedi[i]] + h(node[current - 1].sosedi[i], y, mm, k, kk);
+				if (!used[node[current - 1].sosedi[i]])
+				{
+					Q.push(node[current - 1].sosedi[i], f[node[current - 1].sosedi[i]]);
+				}
+			}
+		}
+	}
+}
+void file_out(vector<int>& c, int** mm, int k, int kk)
+{
+	ofstream fout;
+	fout.open("d:\\output.txt");
+	bool is;
+	if (curr == 0) { cout << "No Path"; }
+	else
+	{
+		for (int i = 0; i < k; i++)
+		{
+			cout << "\n | ";
+			for (int j = 0; j < kk; j++)
+			{
+				is = false;
+				for (unsigned int o = 0; o < c.size(); o++)
+				{
+					if (mm[i][j] == c[o])
+					{
+						is = true;
+
+						cout << symb(o - 1) << ' '; fout << symb(o - 1) << ' ';
+						break;
+					}
+				}
+				if (is == false)
+				{
+					if (mm[i][j] == 0) { cout << "X ";	fout << "X "; }
+					else { cout << "  ";	fout << "  "; }
+				}
+			}
+			fout << endl;
+			cout << "|";
+		}
+	}
+}
+void sol(Vertex* mVert, int** mm, int k, int kk)
+{
 	for (int i = 0; i < k; i++)
 	{
 		for (int j = 0; j < kk; j++)
 		{
 			if (mm[i][j] >= 1)
 			{
+				mVert[mm[i][j] - 1].i = i;
+				mVert[mm[i][j] - 1].j = j;
 				//For diagonal movement
 				if (i + 1 < k && j + 1 < kk)
 				{
@@ -260,24 +279,14 @@ int main()
 			}
 		}
 	}
-	int* prev = new int[ss];
-	for (int i = 0; i < ss; i++)
-	{
-		prev[i] = 0;
-	}
-	vector<int>c;
-	A_star(mVert, k, kk, mm, prev, ss, Sv, Ev);
-	reconstructpath(prev, c, Sv, Ev);
-	file_out(c, mm, k, kk);
 }
-
 void lout(int** mm, int k, int kk) // вивід оцифрованого лабіринту з номерами вершин
 {
 	cout << " -\n";
 	for (int i = 0; i < k; i++)
 	{
 		cout << " ";
-		for (int j = 0; j < kk; j++) cout << mm[i][j] << "\t";
+		for (int j = 0; j < kk; j++) cout << mm[i][j] << " \t";
 		cout << "\n";
 	}
 	cout << " -\n";
@@ -288,12 +297,10 @@ char symb(int N)
 	{
 		N -= 60;
 	}
-	if (N == 0) return '!';//start
+	if (N == -1) return '!';//0 //start
 	else if (N < 10) return N + 48;
 	else if (N + 87 <= 122) return N + 87;
 	else if (N + 29 <= 87) return N + 29; //для - 'X'
 	else if (N + 30 <= 90) return N + 30;
 	return '+';
 }
-
-
